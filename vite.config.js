f033d105cf7af4f349b9cfa08f01074f20ac8d1f
Dev-Sarmad <sarmadh230@gmail.com>
2024-01-19 04:07:33 +0500

    setting viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                fetch: resolve(__dirname, 'details.html'),
-                key: resolve(__dirname, 'filename.html'),
-
-            }
-        }
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        details: resolve(__dirname, "details.html"),
+      },
     },
+  },
 });
